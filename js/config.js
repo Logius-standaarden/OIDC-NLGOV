e82d5a5097e7cb872ec2d8e03ccf6c34e64aaeaa
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2025-04-30 11:32:22 +0200

    Beheer: update companyURL voor Logius (#30)
    Relates to Logius-standaarden/Automatisering#23

--- js/config.js
+++ js/config.js
@@ -18,27 +18,27 @@ let respecConfig = {
       {
         name: "Remco Schaar",
         company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
+        companyURL: "https://logius.nl",
       },
       {
         name: "Frank van Es",
         company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
+        companyURL: "https://logius.nl",
       },
       {
         name: "Pieter Hering",
         company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
+        companyURL: "https://logius.nl",
       },
       {
         name: "Martin van der Plas",
         company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
+        companyURL: "https://logius.nl",
        },
       {
         name: "Alexander Green",
         company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
+        companyURL: "https://logius.nl",
       }
     ],
   authors:
@@ -46,12 +46,12 @@ let respecConfig = {
       {
         name: "Remco Schaar",
         company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
+        companyURL: "https://logius.nl",
       },
       {
         name: "Frank van Es",
         company: "Logius",
-        companyURL: "https://github.com/Logius-standaarden",
+        companyURL: "https://logius.nl",
       },
       {
         name: "Joris Joosten",
