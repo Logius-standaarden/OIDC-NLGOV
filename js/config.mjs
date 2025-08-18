f5a7dea435fde74aeb9e6a072fc9cceb7fa438ab
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2025-08-18 14:21:18 +0200

    Beheer: gebruik gepubliceerde Mermaid plugin (#39)

--- js/config.mjs
+++ js/config.mjs
@@ -1,6 +1,5 @@
 import { loadRespecWithConfiguration } from "https://logius-standaarden.github.io/publicatie/respec/organisation-config.mjs";
-import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs';
-mermaid.initialize({ startOnLoad: false });
+import { generateMermaidFigures } from "https://logius-standaarden.github.io/publicatie/respec/plugins/mermaid.mjs";
 
 loadRespecWithConfiguration({
   useLogo: true,
@@ -251,27 +250,5 @@ loadRespecWithConfiguration({
     },
   },
 
-  postProcess: [
-    async (config, document, utils) => {
-      const mermaidFigures = document.querySelectorAll('.mermaid');
-      const generatedFigures = [];
-      for (const figure of mermaidFigures) {
-        const figureName = figure.dataset.figureName;
-        if (figureName) {
-          const fetchSource = await fetch(`./media/${figureName}`);
-          const preElement = document.createElement('pre');
-          preElement.classList.add('mermaid');
-          preElement.prepend(document.createTextNode(await fetchSource.text()));
-          figure.replaceWith(preElement);
-          generatedFigures.push(preElement);
-        } else {
-          generatedFigures.push(figure);
-        }
-      }
-      mermaid.run({
-        nodes: generatedFigures,
-      });
-    }
-  ]
-
+  postProcess: [ generateMermaidFigures ]
 });
